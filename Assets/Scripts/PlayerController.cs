5b0e62a92d1359413d3f32dd308e7fadc0dc086a
Aria Akbariyeh <aria.ak@hotmail.com>
2024-08-09 02:12:11 -0400

    Add boost using left shift

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+
     [SerializeField] float torqueAmount = 1;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 15f;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        } else {
+            surfaceEffector2D.speed = baseSpeed;
+            
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
         }
 
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
         }
